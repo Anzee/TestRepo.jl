ca0f872a76410f4c86402013d26dbf81168b2394
Anssi Aska <anssi.aska@gmail.com>
2018-11-01 15:32:03 +0200

    added lsq

--- src/TestRepo.jl
+++ src/TestRepo.jl
@@ -1,3 +1,9 @@
 module TestRepo
-println("loaded")
+    function lsq_fit(x,y)
+        n=length(x)
+        X=[ones(n,1) x]
+        return (X'*X)^(-1)*X'*y
+    end
+    println("loaded")
+    export lsq_fit
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,6 @@
-using Test
-@test 1==1
+using Test, TestRepo
+
+x=[0.0,2.0,4.0]
+y=[0.0,3.0,5.0]
+a0,a1=lsq_fit(x,y)
+@test isapprox([a0,a1],[1/6,1.25])
